8eeec7792983873c54e9d4c92260d7cec7e6f38e
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2023-05-03 21:35:36 +0200

    Update

--- src/vista/InicioCarga.java
+++ src/vista/InicioCarga.java
@@ -34,7 +34,7 @@ public class InicioCarga extends javax.swing.JFrame {
 
         jPanel1.setPreferredSize(new java.awt.Dimension(820, 480));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Inicio (1).gif"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\joans\\Downloads\\Inicio (1).gif")); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
